a860891657ed575ac5d298b165d83bacf354a834
Sciencekill <119404250+sciencekiller@users.noreply.github.com>
2024-10-07 22:13:37 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,9 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "dockerhub.sciencekill.top": dockerHub,
+  "ghcr.sciencekill.top": "https://ghcr.io",
 
-  // staging
-  "docker-staging.libcuda.so": dockerHub,
 };
 
 function routeByHosts(host) {
